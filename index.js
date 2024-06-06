4bb4e82de3c1addb5dbcdd421458fa08aa17dddc
Matayo Ayo <mathewfribergayo@gmail.com>
2024-06-06 18:51:34 +0300

    Restored to previous verision to use nin and method

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fetcher = require('./lib/nida-tz')
 
 module.exports = {
-  nidaData: async function (nin) {
+  nidaData: async function (nin, method) {
     try {
-      return await fetcher.getNin(nin)
+      if (!(method === 'post' || method === 'get')) {
+        throw new Error("Invalid http method")
+      } else return await fetcher.getNin(nin, method)
     }
     catch (err) {
       return err.message
